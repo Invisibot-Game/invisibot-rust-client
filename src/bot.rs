d3d99bbf4c42f8ecd2283e298c72718ae9a25fab
Vidar Magnusson <git@vidarmagnusson.com>
2023-04-30 00:08:30 +0200

    Add some RNG to movement

--- src/bot.rs
+++ src/bot.rs
@@ -3,9 +3,10 @@ use invisibot_game::{
     game_logic::game_map::TileType,
     utils::{coordinate::Coordinate, direction::Direction},
 };
+use rand::seq::SliceRandom;
 
 pub fn handle_round(game_round: &GameRound, prev_move: &Option<Direction>) -> Option<Direction> {
-    let dir = get_free_directions(game_round)
+    let mut available_dirs: Vec<Direction> = get_free_directions(game_round)
         .into_iter()
         .filter(|d| {
             if let Some(m) = prev_move {
@@ -14,8 +15,12 @@ pub fn handle_round(game_round: &GameRound, prev_move: &Option<Direction>) -> Op
                 true
             }
         })
-        .last();
-    dir
+        .collect();
+
+    let mut rng = rand::thread_rng();
+    available_dirs.shuffle(&mut rng);
+
+    available_dirs.first().map(|d| d.clone())
 }
 
 fn get_free_directions(game_round: &GameRound) -> Vec<Direction> {
